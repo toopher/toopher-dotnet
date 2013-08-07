7f7ab4802b00dce63d8858d1be6f6e56b7df5100
Seth Holloway <smholloway@gmail.com>
2013-08-07 11:27:30 -0500

    Add Travis with test stub

---
+++ ToopherDotNet/ToopherDotNetTests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace ToopherDotNet
+{
+	[TestFixture()]
+	public class ToopherDotNetTests
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
